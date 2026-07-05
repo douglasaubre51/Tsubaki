aa8eddaf356301dc170068547410326e8fd3bad4
Allen Bose <boseallen192@gmail.com>
2026-07-05 20:54:44 +0530

    Added icons for tabbar

--- Tsubaki/Tsubaki/MainPage.xaml.cs
+++ Tsubaki/Tsubaki/MainPage.xaml.cs
@@ -20,45 +20,45 @@ public partial class MainPage : ContentPage
         var context = BindingContext as MainPageViewModel;
 
         // Initialize Remote EnvKeys!
-        await Dispatcher.DispatchAsync(async () => await InitEnvKeys());
+        //await Dispatcher.DispatchAsync(async () => await InitEnvKeys());
 
         await Dispatcher.DispatchAsync(() =>
             context!.IsPageLoading = true
         );
     }
 
-    async Task InitEnvKeys()
-    {
-        try
-        {
-            if (Storage.SecureStorage.IsEnvKeysInitialized() is true) return;
+    //async Task InitEnvKeys()
+    //{
+    //    try
+    //    {
+    //        if (Storage.SecureStorage.IsEnvKeysInitialized() is true) return;
 
-            string productKey = await Storage.SecureStorage.GetProductKey();
-            if (productKey == string.Empty)
-            {
-                await Storage.SecureStorage.StoreProductKey();
-                productKey = await Storage.SecureStorage.GetProductKey();
-            }
+    //        string productKey = await Storage.SecureStorage.GetProductKey();
+    //        if (productKey == string.Empty)
+    //        {
+    //            await Storage.SecureStorage.StoreProductKey();
+    //            productKey = await Storage.SecureStorage.GetProductKey();
+    //        }
 
-            string projectKey = Storage.SecureStorage.GetProjectKey();
+    //        string projectKey = Storage.SecureStorage.GetProjectKey();
 
-            ClientActivationStatusEnum status = await _misatoApiServ.CheckClientActivationStatus(projectKey, productKey);
-            if (status == ClientActivationStatusEnum.FAILURE) return;
+    //        ClientActivationStatusEnum status = await _misatoApiServ.CheckClientActivationStatus(projectKey, productKey);
+    //        if (status == ClientActivationStatusEnum.FAILURE) return;
 
-            if (status == ClientActivationStatusEnum.NOT_CREATED)
-                await _misatoApiServ.CreateNewClient(productKey);
+    //        if (status == ClientActivationStatusEnum.NOT_CREATED)
+    //            await _misatoApiServ.CreateNewClient(productKey);
 
-            List<AttributeResponseDto> attributes = await _misatoApiServ.FetchENVKeys(projectKey, productKey);
-            if (attributes.Count is 0) return;
+    //        List<AttributeResponseDto> attributes = await _misatoApiServ.FetchENVKeys(projectKey, productKey);
+    //        if (attributes.Count is 0) return;
 
-            foreach (var attribute in attributes)
-                await Storage.SecureStorage.SetENVKeys(attribute.Key, attribute.Value);
+    //        foreach (var attribute in attributes)
+    //            await Storage.SecureStorage.SetENVKeys(attribute.Key, attribute.Value);
 
-            Storage.SecureStorage.SetIsEnvKeysInitialized(true);
-        }
-        catch (Exception ex)
-        {
-            Debug.WriteLine("Env Keys init error: " + ex.Message);
-        }
-    }
+    //        Storage.SecureStorage.SetIsEnvKeysInitialized(true);
+    //    }
+    //    catch (Exception ex)
+    //    {
+    //        Debug.WriteLine("Env Keys init error: " + ex.Message);
+    //    }
+    //}
 }
